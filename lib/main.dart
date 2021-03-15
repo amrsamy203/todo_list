3840c842aade7be75b814bceb87ed00049063bee
Amr Samy <amr.samy@smartlink.me>
2021-03-15 18:26:41 +0200

    implement newToDo Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo_list/screens/newTodo.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,23 +10,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      initialRoute: '/',
+      routes: {
+        '/' : (context) => AddTodo(),
+      },
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        body: Center(
-          child: Text('Welcome ToDo App'),
-        ),
       ),
     );
   }
--- lib/screens/newTodo.dart
+++ lib/screens/newTodo.dart
@@ -1,3 +1,4 @@
+import 'dart:html';
 
 import 'package:flutter/material.dart';
 
@@ -6,65 +7,100 @@ class AddTodo extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'New TODO',
-          style: Theme.of(context).textTheme.headline1,
+        appBar: AppBar(
+          title: Text(
+            'New TODO',
+          ),
         ),
-      ),
-      body: ListView(
+        body: ListView(
+          children: [
+            /*Container(
+            padding: EdgeInsets.only(left: 100.0, top: 16.0),
+            child: Text('amr'),
+          ),*/
+            TypeSection(),
+            ContentSection(),
+          // ButtonSection,
+          ],
+        ));
+  }
+}
+
+class TypeSection extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(32),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          TypeSection,
-          ContentSection,
-          ButtonSection,
+          Text('Category', style: Theme.of(context).textTheme.headline6),
+          SizedBox(
+            height: 15,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              _buildButtonColumn(Icons.card_travel, "TRAVEL"),
+              _buildButtonColumn(Icons.alarm, 'ALARM'),
+              _buildButtonColumn(Icons.music_note_rounded, 'MUSIC'),
+              _buildButtonColumn(Icons.work_rounded, 'WORK'),
+              _buildButtonColumn(Icons.home_rounded, 'HOME'),
+              _buildButtonColumn(Icons.settings_rounded, 'OTHER')
+            ],
+          ),
         ],
-      )
+      ),
     );
   }
 
-  Widget TypeSection = Container(
-    padding: EdgeInsets.all(32),
-    child: Column(
-      children: [
-        Text('Type'),
-        SizedBox(height: 10,),
-        Row(
-          children: [
-            _buildButtonColumn(Icons.card_travel, "TRAVEL"),
-            _buildButtonColumn(Icons.alarm, 'TRAVEL'),
-            _buildButtonColumn(Icons.music_note_rounded, 'MUSIC')
-          ],
-        ),
-        SizedBox(height: 10,),
-        Row(
-          children: [
-            _buildButtonColumn(Icons.work_rounded, 'WORK'),
-            _buildButtonColumn(Icons.home_rounded, 'HOME'),
-            _buildButtonColumn(Icons.settings_rounded, 'OTHER')
-          ],
-        ),
-      ],
-    ),
-  );
-
   Column _buildButtonColumn(IconData icon, String label) {
     return Column(
-      mainAxisSize: MainAxisSize.min,
-      mainAxisAlignment: MainAxisAlignment.center,
+      /*mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,*/
       children: [
-        //Icon(icon, color: color),
+        Icon(icon),
         Container(
           margin: const EdgeInsets.only(top: 8),
           child: Text(
             label,
             style: TextStyle(
-                fontSize: 12,
-                fontWeight: FontWeight.w400,
-                //color: color
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              //color: color
             ),
           ),
         )
       ],
     );
   }
+}
+
+class ContentSection extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      padding: EdgeInsets.only(left: 32, right: 32),
+      child: Column(
+        children: [
+          TextFormField(
+            decoration: InputDecoration(
+              hintText: 'Title',
+            ),
+          ),
+          Padding(padding: EdgeInsets.all(32)),
+          TextField(
+            decoration: InputDecoration(
+              hintText: 'Description',
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.all(new Radius.circular(25.0)),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
 }
